6b7b216c110f03eed8af8af10edae0f0448a0a4b
lanijw <lani.wagner@edu.tbz.ch>
2019-03-15 16:13:56 +0100

    Initialization of springboot

---
+++ src/main/java/ch/nyp/schnuppertag_software/SchnuppertagSoftwareApplication.java
@@ -0,0 +1,13 @@
+package ch.nyp.schnuppertag_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchnuppertagSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchnuppertagSoftwareApplication.class, args);
+	}
+
+}
