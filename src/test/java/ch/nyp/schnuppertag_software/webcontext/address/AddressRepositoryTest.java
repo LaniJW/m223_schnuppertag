d9f97c4c9f7a7ad7e375b9324aafa2107ea4e322
Alex <Alexandra.girsberger@nyp.ch>
2019-03-28 11:29:49 +0100

    Add Controller, Repository & Service Test Class

---
+++ src/test/java/ch/nyp/schnuppertag_software/webcontext/address/AddressRepositoryTest.java
@@ -0,0 +1,47 @@
+package ch.nyp.schnuppertag_software.webcontext.address;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 
+ * @author Alexandra Girsberger
+ * @since 2019-03-28
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@TestPropertySource("/application-test.properties")
+@DataJpaTest
+
+public class AddressRepositoryTest {
+
+	@Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private DataHolder<Address> dataHolder;
+    
+    @Before
+    public void setup() {
+    		   
+    }
+
+    @Test
+    public void findAll_returnsAll() {
+       assertThat(addressRepository.findAll()).containsExactlyElementsOf(dataHolder.asLisz());
+    }  
+}
